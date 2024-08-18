1206d1ee00ed58fda7b97cfb9c089e185e0262b4
alfiaditya <alfiaditya12@gmail.com>
2024-08-19 00:37:16 +0700

    keempat

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize} from "sequelize";
 
-const db = new Sequelize('ternak_db', 'root', '', {
+const db = new Sequelize('ternak_db', 'root', 'root12345', {
     host: 'localhost',
     dialect: 'mysql'
 });
